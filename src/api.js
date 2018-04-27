c3cfdee5300240d7aa26fe432bfd46fc420fdb7e
Ted Leary <markeleary@gmail.com>
2018-04-27 11:12:01 -0700

    add process.env.API_KEY in place of api key in api call, add .env file, add .env to .gitignore

--- src/api.js
+++ src/api.js
@@ -5,7 +5,7 @@ import $ from 'jquery';
 
 export function apiCall(displaySearch, searchInput, searchError) {
 
-  $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${searchInput}&location=or-portland&skip=0&limit=25&user_key=76773493dc43790b2f7bbcb11b51a0b3`).then(function(response){
+  $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${searchInput}&location=or-portland&skip=0&limit=25&user_key=${process.env.API_KEY}`).then(function(response){
     console.log(response);
     displaySearch(response);
   }).fail(function(){
