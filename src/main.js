5532e9c9d59062299f846c87a37601941907e129
Ted Leary <markeleary@gmail.com>
2018-04-27 11:00:51 -0700

    add areTheyAcceptingNewPatients conditional, add same to appended list

--- src/main.js
+++ src/main.js
@@ -11,14 +11,21 @@ let displaySearch = function(response){
     $("#output").text("Sorry, your search criteria does not return any results.")
   }
   for (var i = 0; i < response.data.length; i++) {
-
+    let areTheyAcceptingNewPatients;
+    console.log(response.data[i].practices[0].accepts_new_patients);
+    if (response.data[i].practices[0].accepts_new_patients === true) {
+      areTheyAcceptingNewPatients = "This doctor is currently accepting new patients."
+    } else {
+      areTheyAcceptingNewPatients = "This doctor is not currently accepting new patients."
+    }
     $("#output").append("<ul>" + "<li>" + "First name: " + response.data[i].profile.first_name + "</li>" +
                                  "<li>" + "Last name: " + response.data[i].profile.last_name + "</li>" +
                                  "<li>" + response.data[i].practices[0].visit_address.city + "</li>" +
                                  "<li>" + response.data[i].practices[0].visit_address.state + "</li>" +
                                  "<li>" + response.data[i].practices[0].visit_address.street + "</li>" +
-                                 "<li>" + response.data[i].practices[0].visit_address.street2 + "</li>" +
                                  "<li>" + response.data[i].practices[0].visit_address.zip + "</li>" +
+                                 "<li>" + "Phone number: " + response.data[i].practices[0].phones[0].number + "</li>" +
+                                 "<li>" + areTheyAcceptingNewPatients + "</li>" +
                                  "</ul>" +
                                  "<p>" + response.data[i].profile.bio + "</p>");
   }
