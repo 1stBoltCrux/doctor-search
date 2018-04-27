3268410a540b250f2f85afadf86c8e92cb8992e0
Ted Leary <markeleary@gmail.com>
2018-04-27 09:50:18 -0700

    change api url to only search within portland or

--- src/api.js
+++ src/api.js
@@ -5,7 +5,7 @@ import $ from 'jquery';
 
 export function apiCall(displaySearch, searchInput, searchError) {
 
-  $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${searchInput}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=76773493dc43790b2f7bbcb11b51a0b3`).then(function(response){
+  $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${searchInput}&location=or-portland&skip=0&limit=25&user_key=76773493dc43790b2f7bbcb11b51a0b3`).then(function(response){
     console.log(response);
     displaySearch(response);
   }).fail(function(){
